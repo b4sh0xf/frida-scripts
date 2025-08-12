004db25a0e1aafcf0c6c336d195c3f2eda4be83e
asmlinkage0x0f <153856077+b4sh0xf@users.noreply.github.com>
2025-08-12 13:22:13 -0300

    Create weakCrypto.js

---
+++ allsafe/weakCrypto.js
@@ -0,0 +1,19 @@
+Java.perform(() => {
+
+  let crypto = Java.use('infosecadventures.allsafe.challenges.WeakCryptography')
+  
+  crypto['encrypt'].implementation = function (a) {
+    console.log(`[+] entry: ${a}`)
+    let res = typeof(a)
+    console.log(`[+] return (mod): ${res}`)
+    return res
+  }
+
+  crypto['md5Hash'].implementation = function (a) {
+    console.log(`[+] entry: ${a}`)
+    let res = this.md5Hash(a)
+    console.log(`[+] hash: ${res}`)
+    return res 
+  }
+
+})
